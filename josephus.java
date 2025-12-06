d25f2e9033b99897370353ba366fa9c8f9c53a4e
Dinesh kumar <tdinesh9d@gmail.com>
2025-12-06 19:05:55 +0530

    Add example of Josephus problem algorithm
    Added a commented-out example of the Josephus problem algorithm.

--- josephus.java
+++ josephus.java
@@ -10,3 +10,22 @@ class Josephus {
         System.out.println("Survivor = "+(josephus(n,k)+1));
     }
 }
+
+/*
+n = 7
+k = 3
+
+a = [1, 2, 3, 4, 5, 6, 7]   # direct initialization
+res = 0
+
+print("Elimination order:")
+
+while n > 1:
+    res = (res + k - 1) % n      # next to die
+    print("Person", a[res], "died")
+
+    a.pop(res)                   # remove person
+    n -= 1
+
+print("Survivor =", a[0])
+*/
